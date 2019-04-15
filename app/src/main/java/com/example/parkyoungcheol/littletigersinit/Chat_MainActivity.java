aa78d680660a5103688b93d1c49964ce05f396cb
kimkyoungsub <kleg26315>
2019-04-16 02:03:49 +0900

    test

--- app/src/main/java/com/example/parkyoungcheol/littletigersinit/Chat_MainActivity.java
+++ app/src/main/java/com/example/parkyoungcheol/littletigersinit/Chat_MainActivity.java
@@ -10,8 +10,8 @@ public class Chat_MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test//
         setContentView(R.layout.activity_chat_main);
-
         getSupportFragmentManager().beginTransaction().replace(R.id.ChatMainActivity_framelayout, new PeopleFragment()).commit();
     }
 }
