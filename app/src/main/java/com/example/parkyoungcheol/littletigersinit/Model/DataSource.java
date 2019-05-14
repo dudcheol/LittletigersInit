be50cde2fb420758e01c77dd835745bf35aac507
dudcheol <parkyc0301@gmail.com>
2019-05-15 02:28:02 +0900

    git pull request test

--- app/src/main/java/com/example/parkyoungcheol/littletigersinit/Model/DataSource.java
+++ app/src/main/java/com/example/parkyoungcheol/littletigersinit/Model/DataSource.java
@@ -140,7 +140,7 @@ public class DataSource {
         switch (source) {
 
             // Test URL(정왕역 좌표) : "http://map.naver.com/search2/interestSpot.nhn?type=CAFE&boundary=126.73278739132492%3B37.34197819069592%3B126.75278739132492%3B37.36197819069592&pageSize=5"
-            // 네이버 웹페이지에서 가져오는 정보
+            // 네이버 웹페이지에서 가져오는 정보임
             case CAFE:
                 ret = "http://map.naver.com/search2/interestSpot.nhn?type=CAFE&boundary=" + Double.toString(lon - 0.01) + "%3B" +
                         Double.toString(lat - 0.01) + "%3B" + Double.toString(lon + 0.01) +
