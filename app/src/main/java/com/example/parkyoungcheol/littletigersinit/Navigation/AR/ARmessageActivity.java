7d45e14edfe49b1a0631da9ac43caac9a2871925
kleg26315 <kleg26315@naver.com>
2019-06-23 13:37:47 +0900

    ARmessage 일반지도길안내 위도,경도 설정 오류 해결

--- app/src/main/java/com/example/parkyoungcheol/littletigersinit/Navigation/AR/ARmessageActivity.java
+++ app/src/main/java/com/example/parkyoungcheol/littletigersinit/Navigation/AR/ARmessageActivity.java
@@ -188,7 +188,7 @@ public class ARmessageActivity extends FragmentActivity implements OnMapReadyCal
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
                                                 String longitude = Double.toString(armsgData.getLongitude());
-                                                String latitude = Double.toString(armsgData.getLongitude());
+                                                String latitude = Double.toString(armsgData.getLatitude());
 
 
                                                 Intent intent = new Intent(ARmessageActivity.this, AR_navigationActivity.class);
